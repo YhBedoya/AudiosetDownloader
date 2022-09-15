66e2927644a4eb95fecf9e6adb01729907809196
Yhorman Bedoya <>
2022-09-15 21:01:36 +0200

    init

--- process.py
+++ process.py
@@ -32,7 +32,8 @@ if __name__ == '__main__':
         csv_dataset='./data/unbalanced_train_segments.csv',
         destination_dir='./output',
         fs=16000,
-        num_partitions=4
+        num_partitions=1,
+        mode = 'partitions'
     )
 
     args = parser.parse_args()
